7a48a89785209c883acd79a18788e2ac800085d9
Andera Singgih Pratama <anderasinggih1@gmail.com>
2026-05-25 03:40:08 +0700

    fix reverb limit

--- config/reverb.php
+++ config/reverb.php
@@ -35,7 +35,7 @@ return [
             'options' => [
                 'tls' => [],
             ],
-            'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
+            'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 1_000_000),
             'scaling' => [
                 'enabled' => env('REVERB_SCALING_ENABLED', false),
                 'channel' => env('REVERB_SCALING_CHANNEL', 'reverb'),
@@ -82,8 +82,8 @@ return [
                 ],
                 'allowed_origins' => ['*'],
                 'ping_interval' => env('REVERB_APP_PING_INTERVAL', 60),
-                'activity_timeout' => env('REVERB_APP_ACTIVITY_TIMEOUT', 30),
-                'max_message_size' => env('REVERB_APP_MAX_MESSAGE_SIZE', 10_000),
+                'activity_timeout' => env('REVERB_APP_ACTIVITY_TIMEOUT', 120),
+                'max_message_size' => env('REVERB_APP_MAX_MESSAGE_SIZE', 1_000_000),
             ],
         ],
 
